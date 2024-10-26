ec5f1979756a00a1130bb6961ecd1bac194f951a
jaco <ventas@jacoagency.io>
2024-10-26 10:26:37 -0700

    repo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // Las Server Actions ahora están disponibles por defecto
+    images: {
+        domains: ['fal.media'],
+    },
 };
 
 module.exports = nextConfig;
